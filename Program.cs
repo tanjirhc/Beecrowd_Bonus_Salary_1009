f6f89938bc06a2a3145ee2e04874b635ef95f3e9
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-10 01:21:52 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_Bonus_Salary_1009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Salary, Sales, Incentive, Total;
+            string name = Console.ReadLine();
+            Salary = Convert.ToDouble(Console.ReadLine());
+            Sales = Convert.ToDouble(Console.ReadLine());
+
+            Incentive = (Sales * 0.15);
+            Total   = Salary + Incentive;
+            
+            Console.WriteLine("TOTAl = R$ {0}", Total.ToString("0.00"));
+            Console.ReadKey();
+        }
+    }
+}
